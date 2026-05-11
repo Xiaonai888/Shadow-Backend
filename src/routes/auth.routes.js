7b59d2c00b280b2c51fa8812fa1045c1631b10dc
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-11 21:02:08 +0700

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,15 @@
 import express from 'express'
-import { adminLogin, checkAdmin } from '../controllers/auth.controller.js'
+import {
+  adminLogin,
+  checkAdmin,
+  changeAdminPassword,
+} from '../controllers/auth.controller.js'
 import { requireAdmin } from '../middleware/auth.middleware.js'
 
 const router = express.Router()
 
 router.post('/login', adminLogin)
 router.get('/me', requireAdmin, checkAdmin)
+router.patch('/change-password', requireAdmin, changeAdminPassword)
 
 export default router
