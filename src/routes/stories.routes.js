e5b577fbbaef9d755c4876e4d885f28f378f3bc7
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-14 21:54:09 +0700

    Update stories.routes.js

--- src/routes/stories.routes.js
+++ src/routes/stories.routes.js
@@ -6,6 +6,7 @@ import {
   getMyStories,
   getStoryById,
   getStoryEpisodes,
+  updateEpisodeStatus,
 } from '../controllers/stories.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
 
@@ -18,5 +19,6 @@ router.get('/:storyId', requireUser, getStoryById)
 router.post('/:storyId/episodes/create', requireUser, createEpisode)
 router.get('/:storyId/episodes', requireUser, getStoryEpisodes)
 router.get('/:storyId/episodes/:episodeId', requireUser, getEpisodeById)
+router.patch('/:storyId/episodes/:episodeId/status', requireUser, updateEpisodeStatus)
 
 export default router
