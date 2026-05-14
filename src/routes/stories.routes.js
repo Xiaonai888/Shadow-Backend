8e7feb19766a0b564d4080e36f0eb13e708d374b
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-14 21:37:40 +0700

    Update stories.routes.js

--- src/routes/stories.routes.js
+++ src/routes/stories.routes.js
@@ -1,8 +1,11 @@
 import express from 'express'
 import {
+  createEpisode,
   createStory,
+  getEpisodeById,
   getMyStories,
   getStoryById,
+  getStoryEpisodes,
 } from '../controllers/stories.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
 
@@ -11,5 +14,9 @@ const router = express.Router()
 router.post('/create', requireUser, createStory)
 router.get('/my', requireUser, getMyStories)
 router.get('/:storyId', requireUser, getStoryById)
+
+router.post('/:storyId/episodes/create', requireUser, createEpisode)
+router.get('/:storyId/episodes', requireUser, getStoryEpisodes)
+router.get('/:storyId/episodes/:episodeId', requireUser, getEpisodeById)
 
 export default router
