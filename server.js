298c9827a1bcbcfbff0fe07ff3b659127b7035e6
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-15 14:32:05 +0700

    Update server.js

--- server.js
+++ server.js
@@ -38,22 +38,27 @@ const allowedOrigins = [
   'http://127.0.0.1:5000',
 ].filter(Boolean)
 
-app.use(
-  cors({
-    origin(origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        return callback(null, true)
-      }
+const corsOptions = {
+  origin(origin, callback) {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true)
+    }
 
-      return callback(new Error(`Not allowed by CORS: ${origin}`))
-    },
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-)
+    return callback(new Error(`Not allowed by CORS: ${origin}`))
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Admin-Name',
+    'X-Admin-Actor',
+    'X-Admin-Id',
+  ],
+}
 
-app.options('*', cors())
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions))
 
 app.use(express.json({ limit: '10mb' }))
 app.use(express.urlencoded({ extended: true, limit: '10mb' }))
