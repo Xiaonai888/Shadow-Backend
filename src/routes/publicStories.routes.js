7328ffbaecef5ea5e0d95d4c30284e10e6f42c90
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-15 07:13:41 +0700

    Update publicStories.routes.js

--- src/routes/publicStories.routes.js
+++ src/routes/publicStories.routes.js
@@ -1,12 +1,14 @@
 import express from 'express'
 import {
   getPublicEpisodeById,
+  getPublicStories,
   getPublicStoryById,
   getPublicStoryEpisodes,
 } from '../controllers/publicStories.controller.js'
 
 const router = express.Router()
 
+router.get('/stories', getPublicStories)
 router.get('/stories/:storyId', getPublicStoryById)
 router.get('/stories/:storyId/episodes', getPublicStoryEpisodes)
 router.get('/stories/:storyId/episodes/:episodeId', getPublicEpisodeById)
