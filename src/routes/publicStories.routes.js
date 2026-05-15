21b8a01cffd580e00fa41dbf53a4eb1e63686b8d
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-15 08:08:12 +0700

    Update publicStories.routes.js

--- src/routes/publicStories.routes.js
+++ src/routes/publicStories.routes.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import {
   getPublicEpisodeById,
+  getPublicShadowExclusiveStories,
+  getPublicShadowExclusiveStoryById,
   getPublicStories,
   getPublicStoryById,
   getPublicStoryEpisodes,
@@ -9,6 +11,9 @@ import {
 const router = express.Router()
 
 router.get('/stories', getPublicStories)
+router.get('/shadow-exclusive/stories', getPublicShadowExclusiveStories)
+router.get('/shadow-exclusive/stories/:storyId', getPublicShadowExclusiveStoryById)
+
 router.get('/stories/:storyId', getPublicStoryById)
 router.get('/stories/:storyId/episodes', getPublicStoryEpisodes)
 router.get('/stories/:storyId/episodes/:episodeId', getPublicEpisodeById)
