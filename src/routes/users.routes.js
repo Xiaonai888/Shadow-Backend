f5c6aa644f5ae9c50ccc343720fc2c5b62340564
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-17 10:31:28 +0700

    Update users.routes.js

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -3,6 +3,7 @@ import {
   getCurrentUser,
   loginUser,
   registerUser,
+  updateUserAvatar,
 } from '../controllers/users.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
 
@@ -11,5 +12,6 @@ const router = express.Router()
 router.post('/register', registerUser)
 router.post('/login', loginUser)
 router.get('/me', requireUser, getCurrentUser)
+router.put('/avatar', requireUser, updateUserAvatar)
 
 export default router
