24d58ad54a55e17dab39c205e913bc267ef44da0
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-17 07:01:02 +0700

    Update authors.routes.js

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {
   createAuthorPage,
   getMyAuthorPage,
+  updateAuthorAvatar,
 } from '../controllers/authors.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
 
@@ -9,5 +10,6 @@ const router = express.Router()
 
 router.get('/me', requireUser, getMyAuthorPage)
 router.post('/create', requireUser, createAuthorPage)
+router.put('/avatar', requireUser, updateAuthorAvatar)
 
 export default router
