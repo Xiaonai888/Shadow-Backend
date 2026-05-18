442e4261beea347241124746a15c3a47fe9a6539
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-18 16:19:23 +0700

    Update adminComments.routes.js

--- src/routes/adminComments.routes.js
+++ src/routes/adminComments.routes.js
@@ -5,13 +5,17 @@ import {
   deleteAdminComment,
   getAdminCommentOwnerReports,
   getAdminComments,
+  getAdminStoryComments,
   moderateAdminComment,
+  searchAdminCommentStories,
 } from '../controllers/adminComments.controller.js'
 
 const router = express.Router()
 
-router.get('/', requireAdmin, getAdminComments)
+router.get('/stories', requireAdmin, searchAdminCommentStories)
+router.get('/story/:storyId', requireAdmin, getAdminStoryComments)
 router.get('/records', requireAdmin, getAdminCommentOwnerReports)
+router.get('/', requireAdmin, getAdminComments)
 router.patch('/:commentId/moderate', requireAdmin, moderateAdminComment)
 router.delete('/:commentId', requireAdmin, deleteAdminComment)
 router.post('/:commentId/ban-user', requireAdmin, banAdminCommentUser)
