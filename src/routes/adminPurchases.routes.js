fde519947fc86f23951d66a35e1ff27555def175
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-19 16:54:57 +0700

    Update adminPurchases.routes.js

--- src/routes/adminPurchases.routes.js
+++ src/routes/adminPurchases.routes.js
@@ -4,10 +4,18 @@ import {
   getAdminPayment,
   getAdminPayments,
 } from '../controllers/adminPayments.controller.js'
+import {
+  confirmAdminManualPayment,
+  getAdminManualPayments,
+  rejectAdminManualPayment,
+} from '../controllers/adminManualPayments.controller.js'
 
 const router = express.Router()
 
 router.get('/', requireAdmin, getAdminPayments)
+router.get('/manual', requireAdmin, getAdminManualPayments)
+router.post('/manual/:paymentId/confirm', requireAdmin, confirmAdminManualPayment)
+router.post('/manual/:paymentId/reject', requireAdmin, rejectAdminManualPayment)
 router.get('/:paymentId', requireAdmin, getAdminPayment)
 
 export default router
