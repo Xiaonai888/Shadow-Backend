2c336052ded5dc28e22c7654b6e24b3d2d0f7729
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-19 11:01:29 +0700

    Update purchase.routes.js

--- src/routes/purchase.routes.js
+++ src/routes/purchase.routes.js
@@ -2,7 +2,6 @@ import express from 'express'
 import { requireUser } from '../middleware/user.middleware.js'
 import {
   createAbaPayment,
-  createPurchaseRequest,
   getAbaPaymentStatus,
   getMyPurchaseRequests,
   getMyWallet,
@@ -15,7 +14,6 @@ const router = express.Router()
 router.get('/packages', getPurchasePackages)
 router.get('/wallet', requireUser, getMyWallet)
 router.get('/requests', requireUser, getMyPurchaseRequests)
-router.post('/requests', requireUser, createPurchaseRequest)
 router.post('/aba/create', requireUser, createAbaPayment)
 router.get('/aba/status/:orderId', requireUser, getAbaPaymentStatus)
 router.post('/aba/callback', handleAbaCallback)
