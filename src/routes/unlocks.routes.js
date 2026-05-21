b08b12bdd087b8531882c7ba79717bc95ef01ed3
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-21 23:19:08 +0700

    Update unlocks.routes.js

--- src/routes/unlocks.routes.js
+++ src/routes/unlocks.routes.js
@@ -2,6 +2,8 @@ import express from 'express'
 import {
   getEpisodeUnlockStatus,
   unlockEpisodeWithDiamonds,
+  unlockEpisodePackageWithDiamonds,
+  unlockEpisodeWithGems,
 } from '../controllers/unlocks.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
 
@@ -9,5 +11,7 @@ const router = express.Router()
 
 router.get('/stories/:storyId/episodes/:episodeId/status', requireUser, getEpisodeUnlockStatus)
 router.post('/stories/:storyId/episodes/:episodeId/diamond', requireUser, unlockEpisodeWithDiamonds)
+router.post('/stories/:storyId/episodes/:episodeId/package', requireUser, unlockEpisodePackageWithDiamonds)
+router.post('/stories/:storyId/episodes/:episodeId/gem', requireUser, unlockEpisodeWithGems)
 
 export default router
