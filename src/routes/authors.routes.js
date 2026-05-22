26094b2c2ab252b4ecc920286be3b3262be8c008
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-22 22:23:48 +0700

    Update authors.routes.js

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -4,11 +4,21 @@ import {
   getMyAuthorPage,
   updateAuthorAvatar,
 } from '../controllers/authors.controller.js'
+import {
+  getMyAuthorIncome,
+  getMyAuthorPaymentMethods,
+  getMyAuthorQuest,
+  saveMyAuthorPaymentMethod,
+} from '../controllers/authorRevenue.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
 
 const router = express.Router()
 
 router.get('/me', requireUser, getMyAuthorPage)
+router.get('/me/quest', requireUser, getMyAuthorQuest)
+router.get('/me/income', requireUser, getMyAuthorIncome)
+router.get('/me/payment-methods', requireUser, getMyAuthorPaymentMethods)
+router.post('/me/payment-methods', requireUser, saveMyAuthorPaymentMethod)
 router.post('/create', requireUser, createAuthorPage)
 router.put('/avatar', requireUser, updateAuthorAvatar)
 
