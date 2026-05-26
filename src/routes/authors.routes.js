22d8baeea1d98209fac41d0447dd8033b77fb1c4
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-26 22:16:53 +0700

    Update authors.routes.js

--- src/routes/authors.routes.js
+++ src/routes/authors.routes.js
@@ -2,7 +2,9 @@ import express from 'express'
 import {
   createAuthorPage,
   getMyAuthorPage,
+  getPublicAuthorPage,
   updateAuthorAvatar,
+  updateAuthorProfileImages,
 } from '../controllers/authors.controller.js'
 import {
   getMyAuthorIncome,
@@ -18,8 +20,10 @@ router.get('/me', requireUser, getMyAuthorPage)
 router.get('/me/quest', requireUser, getMyAuthorQuest)
 router.get('/me/income', requireUser, getMyAuthorIncome)
 router.get('/me/payment-methods', requireUser, getMyAuthorPaymentMethods)
+router.get('/page/:pageUsername', getPublicAuthorPage)
 router.post('/me/payment-methods', requireUser, saveMyAuthorPaymentMethod)
 router.post('/create', requireUser, createAuthorPage)
 router.put('/avatar', requireUser, updateAuthorAvatar)
+router.put('/profile-images', requireUser, updateAuthorProfileImages)
 
 export default router
