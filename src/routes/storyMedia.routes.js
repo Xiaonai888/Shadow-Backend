a17c0bb3abb6476ead13b949b5f5aefc9bccd917
Xiaonai <xiaonai.ns1@gmail.com>
2026-05-29 22:05:58 +0700

    Update storyMedia.routes.js

--- src/routes/storyMedia.routes.js
+++ src/routes/storyMedia.routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import multer from 'multer'
 import { uploadStoryImage } from '../controllers/storyMedia.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
+import tasksRoutes from './src/routes/tasks.routes.js'
 
 const router = express.Router()
 
@@ -11,6 +12,8 @@ const upload = multer({
     fileSize: 8 * 1024 * 1024,
   },
 })
+
+app.use('/api/tasks', tasksRoutes)
 
 router.post('/upload-image', requireUser, upload.single('image'), uploadStoryImage)
 
