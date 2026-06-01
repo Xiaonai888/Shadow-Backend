4f23ed00e184868dc4c5dad7864fe9d071f23c0d
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-01 08:58:34 +0700

    Update auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import {
+  adminForgotPassword,
   adminLogin,
+  adminResetPassword,
   checkAdmin,
   changeAdminPassword,
 } from '../controllers/auth.controller.js'
@@ -9,6 +11,8 @@ import { requireAdmin } from '../middleware/auth.middleware.js'
 const router = express.Router()
 
 router.post('/login', adminLogin)
+router.post('/admin-forgot-password', adminForgotPassword)
+router.post('/admin-reset-password', adminResetPassword)
 router.get('/me', requireAdmin, checkAdmin)
 router.patch('/change-password', requireAdmin, changeAdminPassword)
 
