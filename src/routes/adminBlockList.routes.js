24b69ac05e2527f056c1ec0ae0717c7f8dd15c30
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-03 20:20:22 +0700

    Create adminBlockList.routes.js

---
+++ src/routes/adminBlockList.routes.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import {
+  createBlockedWord,
+  deleteBlockedWord,
+  getBlockedWords,
+  updateBlockedWord,
+} from '../controllers/adminBlockList.controller.js'
+import { requireAdmin } from '../middleware/auth.middleware.js'
+
+const router = express.Router()
+
+router.get('/words', requireAdmin, getBlockedWords)
+router.post('/words', requireAdmin, createBlockedWord)
+router.patch('/words/:wordId', requireAdmin, updateBlockedWord)
+router.delete('/words/:wordId', requireAdmin, deleteBlockedWord)
+
+export default router
