243302e2bcfe1584a2b34a2fa4b012703fd9b749
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-04 10:08:21 +0700

    Update adminBlockList.routes.js

--- src/routes/adminBlockList.routes.js
+++ src/routes/adminBlockList.routes.js
@@ -2,6 +2,7 @@ import express from 'express'
 import {
   createBlockedWord,
   deleteBlockedWord,
+  getBlockedWordRecords,
   getBlockedWords,
   updateBlockedWord,
 } from '../controllers/adminBlockList.controller.js'
@@ -10,6 +11,7 @@ import { requireAdmin } from '../middleware/auth.middleware.js'
 const router = express.Router()
 
 router.get('/words', requireAdmin, getBlockedWords)
+router.get('/records', requireAdmin, getBlockedWordRecords)
 router.post('/words', requireAdmin, createBlockedWord)
 router.patch('/words/:wordId', requireAdmin, updateBlockedWord)
 router.delete('/words/:wordId', requireAdmin, deleteBlockedWord)
