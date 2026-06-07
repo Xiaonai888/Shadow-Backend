f51606699acfb94d88ead15706d4fe6f44a243c1
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-07 19:53:56 +0700

    Update comments.routes.js

--- src/routes/comments.routes.js
+++ src/routes/comments.routes.js
@@ -4,6 +4,7 @@ import {
   getMyCommentActivities,
   getStoryComments,
   moderateComment,
+  toggleCommentLike,
   updateOwnComment,
 } from '../controllers/comments.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
@@ -13,6 +14,7 @@ const router = express.Router()
 router.get('/me/activities', requireUser, getMyCommentActivities)
 router.get('/story/:storyId', getStoryComments)
 router.post('/story/:storyId', requireUser, createStoryComment)
+router.post('/:commentId/like', requireUser, toggleCommentLike)
 router.patch('/:commentId', requireUser, updateOwnComment)
 router.patch('/:commentId/moderate', requireUser, moderateComment)
 
