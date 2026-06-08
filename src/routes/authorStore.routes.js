032a9ee1bf74feda707417a5bf47bada20c71479
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-08 21:56:19 +0700

    Update authorStore.routes.js

--- src/routes/authorStore.routes.js
+++ src/routes/authorStore.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import {
   createMyAuthorStoreProduct,
+  deleteMyAuthorStoreProduct,
   getMyAuthorStoreProducts,
   getPublicAuthorStoreProducts,
+  updateMyAuthorStoreProduct,
 } from '../controllers/authorStore.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
 
@@ -10,6 +12,8 @@ const router = express.Router()
 
 router.get('/me/products', requireUser, getMyAuthorStoreProducts)
 router.post('/me/products', requireUser, createMyAuthorStoreProduct)
+router.put('/me/products/:productId', requireUser, updateMyAuthorStoreProduct)
+router.delete('/me/products/:productId', requireUser, deleteMyAuthorStoreProduct)
 router.get('/page/:pageUsername/products', getPublicAuthorStoreProducts)
 
 export default router
