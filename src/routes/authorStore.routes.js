e55753e53e87f80c49a1c8c146a215ba00df08cb
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-09 14:36:36 +0700

    Update authorStore.routes.js

--- src/routes/authorStore.routes.js
+++ src/routes/authorStore.routes.js
@@ -10,6 +10,7 @@ import {
   getMyAuthorStoreProducts,
   getPublicAuthorStoreProducts,
   reorderMyAuthorStoreCategories,
+  updateMyAuthorStoreCategory,
   updateMyAuthorStoreProduct,
 } from '../controllers/authorStore.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
@@ -20,6 +21,7 @@ router.get('/me/products', requireUser, getMyAuthorStoreProducts)
 router.get('/me/categories', requireUser, getMyAuthorStoreCategories)
 router.post('/me/categories', requireUser, createMyAuthorStoreCategory)
 router.patch('/me/categories/reorder', requireUser, reorderMyAuthorStoreCategories)
+router.patch('/me/categories/:categoryId', requireUser, updateMyAuthorStoreCategory)
 router.delete('/me/categories/:categoryId', requireUser, deleteMyAuthorStoreCategory)
 router.post('/me/products', requireUser, createMyAuthorStoreProduct)
 router.get('/me/orders', requireUser, getMyAuthorStoreOrders)
