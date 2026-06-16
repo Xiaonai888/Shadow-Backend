fdea9e883c0d9e1553096db5bb1c9173d3ebe4cd
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-16 23:28:31 +0700

    Update adminCommunity.routes.js

--- src/routes/adminCommunity.routes.js
+++ src/routes/adminCommunity.routes.js
@@ -3,6 +3,8 @@ import {
   getAdminCommunityAuthors,
   getAdminCommunityOverview,
   getAdminCommunityReaders,
+  getAdminCommunityVisitorOverview,
+  getAdminCommunityVisitors,
 } from '../controllers/adminCommunity.controller.js'
 import { requireAdmin } from '../middleware/auth.middleware.js'
 
@@ -11,5 +13,7 @@ const router = express.Router()
 router.get('/overview', requireAdmin, getAdminCommunityOverview)
 router.get('/readers', requireAdmin, getAdminCommunityReaders)
 router.get('/authors', requireAdmin, getAdminCommunityAuthors)
+router.get('/visitors/overview', requireAdmin, getAdminCommunityVisitorOverview)
+router.get('/visitors', requireAdmin, getAdminCommunityVisitors)
 
 export default router
