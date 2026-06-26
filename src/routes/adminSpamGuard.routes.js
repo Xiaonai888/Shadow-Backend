4ff0071885e3a6c69a36dc1d8ac6261bc865a2f9
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-26 13:09:28 +0700

    Update adminSpamGuard.routes.js

--- src/routes/adminSpamGuard.routes.js
+++ src/routes/adminSpamGuard.routes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import { requireAdmin } from '../middleware/auth.middleware.js'
 import {
+  blockAdminSpamGuardPermanently,
   getAdminSpamGuardEvents,
   getAdminSpamGuardOverview,
   getAdminSpamGuardStates,
   releaseAdminSpamGuardCooldown,
+  releaseAdminSpamGuardQuarantine,
+  unblockAdminSpamGuardPermanent,
 } from '../controllers/adminSpamGuard.controller.js'
 
 const router = express.Router()
@@ -15,5 +18,8 @@ router.get('/overview', getAdminSpamGuardOverview)
 router.get('/states', getAdminSpamGuardStates)
 router.get('/events', getAdminSpamGuardEvents)
 router.patch('/states/:stateId/release', releaseAdminSpamGuardCooldown)
+router.patch('/states/:stateId/release-quarantine', releaseAdminSpamGuardQuarantine)
+router.patch('/states/:stateId/permanent-block', blockAdminSpamGuardPermanently)
+router.patch('/states/:stateId/unblock', unblockAdminSpamGuardPermanent)
 
 export default router
