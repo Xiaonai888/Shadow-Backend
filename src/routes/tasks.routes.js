201d65d156875a408afe4017bc393045b78df222
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-27 22:15:17 +0700

    Update tasks.routes.js

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,15 +1,18 @@
 import express from 'express'
 import { requireUser } from '../middleware/user.middleware.js'
 import {
+  claimRewardChest,
   claimTaskCheckIn,
+  getRewardChest,
   getTaskCheckIn,
   getTaskHistory,
 } from '../controllers/tasks.controller.js'
-
 const router = express.Router()
 
 router.get('/check-in', requireUser, getTaskCheckIn)
 router.post('/check-in/claim', requireUser, claimTaskCheckIn)
+router.get('/reward-chest', requireUser, getRewardChest)
+router.post('/reward-chest/claim', requireUser, claimRewardChest)
 router.get('/history', requireUser, getTaskHistory)
 
 export default router
