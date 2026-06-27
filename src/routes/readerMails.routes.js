93615e9c70dbe18002c200caa5afefc0bf064aca
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-27 10:49:34 +0700

    Update readerMails.routes.js

--- src/routes/readerMails.routes.js
+++ src/routes/readerMails.routes.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import {
   claimMailReward,
+  getDailyCheckInReminder,
   getMyMailUnreadCount,
   getMyMails,
   markMailAsRead,
+  runDailyCheckInReminderMails,
+  updateDailyCheckInReminder,
 } from '../controllers/readerMails.controller.js'
 import { requireUser } from '../middleware/user.middleware.js'
 
@@ -11,6 +14,9 @@ const router = express.Router()
 
 router.get('/', requireUser, getMyMails)
 router.get('/unread-count', requireUser, getMyMailUnreadCount)
+router.get('/daily-checkin-reminder', requireUser, getDailyCheckInReminder)
+router.patch('/daily-checkin-reminder', requireUser, updateDailyCheckInReminder)
+router.post('/daily-checkin-reminder/run', runDailyCheckInReminderMails)
 router.patch('/:mailId/read', requireUser, markMailAsRead)
 router.patch('/:mailId/claim', requireUser, claimMailReward)
 
