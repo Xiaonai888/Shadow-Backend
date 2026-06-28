13dc807028f9842acaae4eea63e396adc4452382
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-28 18:58:59 +0700

    Update adminTwoFactor.routes.js

--- src/routes/adminTwoFactor.routes.js
+++ src/routes/adminTwoFactor.routes.js
@@ -1,6 +1,8 @@
 import express from 'express'
 import { requireAdmin } from '../middleware/auth.middleware.js'
 import {
+  disableEmailOtp,
+‌  enableEmailOtp,
   disableTwoFactor,
   getTwoFactorEvents,
   getTwoFactorStatus,
@@ -16,6 +18,8 @@ router.use(requireAdmin)
 router.get('/status', getTwoFactorStatus)
 router.post('/authenticator/setup-start', startAuthenticatorSetup)
 router.post('/authenticator/setup-verify', verifyAuthenticatorSetup)
+router.post('/email/enable', enableEmailOtp)
+router.post('/email/disable', disableEmailOtp)
 router.post('/disable', disableTwoFactor)
 router.post('/recovery-codes/regenerate', regenerateRecoveryCodes)
 router.get('/events', getTwoFactorEvents)
