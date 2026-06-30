cd128f8098817d2b2628622232f9243f2efb53f5
Xiaonai <xiaonai.ns1@gmail.com>
2026-06-30 22:55:43 +0700

    Update tasks.routes.js

--- src/routes/tasks.routes.js
+++ src/routes/tasks.routes.js
@@ -1,24 +1,37 @@
 import express from 'express'
 import { requireUser } from '../middleware/user.middleware.js'
 import {
+  claimReadingMissionReward,
   claimReadingReward,
   claimRewardChest,
   claimTaskCheckIn,
+  getReadingMission,
+  getReadingMissions,
   getReadingReward,
   getRewardChest,
   getTaskCheckIn,
   getTaskHistory,
+  trackReadingMissionProgress,
   trackReadingRewardProgress,
 } from '../controllers/tasks.controller.js'
+
 const router = express.Router()
 
 router.get('/check-in', requireUser, getTaskCheckIn)
 router.post('/check-in/claim', requireUser, claimTaskCheckIn)
+
 router.get('/reward-chest', requireUser, getRewardChest)
 router.post('/reward-chest/claim', requireUser, claimRewardChest)
+
 router.get('/reading-reward', requireUser, getReadingReward)
 router.post('/reading-reward/progress', requireUser, trackReadingRewardProgress)
 router.post('/reading-reward/claim', requireUser, claimReadingReward)
+
+router.get('/reading-missions', requireUser, getReadingMissions)
+router.get('/reading-missions/:missionId', requireUser, getReadingMission)
+router.post('/reading-missions/:missionId/progress', requireUser, trackReadingMissionProgress)
+router.post('/reading-missions/:missionId/claim', requireUser, claimReadingMissionReward)
+
 router.get('/history', requireUser, getTaskHistory)
 
 export default router
