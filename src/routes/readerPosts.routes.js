cbb97128a41c1e4f255106c076f33f7f43162968
Xiaonai <xiaonai.ns1@gmail.com>
2026-07-19 08:14:32 +0700

    Update readerPosts.routes.js

--- src/routes/readerPosts.routes.js
+++ src/routes/readerPosts.routes.js
@@ -7,11 +7,18 @@ import {
   getReaderPostsFeed,
   updateMyReaderPost,
 } from '../controllers/readerPosts.controller.js'
-import { requireUser } from '../middleware/user.middleware.js'
 import {
   getReaderPostReactionStatus,
   setReaderPostReaction,
 } from '../controllers/readerPostReactions.controller.js'
+import {
+  createReaderPostComment,
+  deleteOwnReaderPostComment,
+  getReaderPostComments,
+  toggleReaderPostCommentLike,
+  updateOwnReaderPostComment,
+} from '../controllers/readerPostComments.controller.js'
+import { requireUser } from '../middleware/user.middleware.js'
 
 const router = express.Router()
 
@@ -20,8 +27,44 @@ router.get('/me', requireUser, getMyReaderPosts)
 router.post('/me', requireUser, createMyReaderPost)
 router.patch('/me/:postId', requireUser, updateMyReaderPost)
 router.delete('/me/:postId', requireUser, deleteMyReaderPost)
-router.get('/:postId/reaction', requireUser, getReaderPostReactionStatus)
-router.post('/:postId/reaction', requireUser, setReaderPostReaction)
+
+router.get(
+  '/:postId/reaction',
+  requireUser,
+  getReaderPostReactionStatus
+)
+router.post(
+  '/:postId/reaction',
+  requireUser,
+  setReaderPostReaction
+)
+
+router.get(
+  '/:postId/comments',
+  requireUser,
+  getReaderPostComments
+)
+router.post(
+  '/:postId/comments',
+  requireUser,
+  createReaderPostComment
+)
+router.patch(
+  '/comments/:commentId',
+  requireUser,
+  updateOwnReaderPostComment
+)
+router.delete(
+  '/comments/:commentId',
+  requireUser,
+  deleteOwnReaderPostComment
+)
+router.post(
+  '/comments/:commentId/like',
+  requireUser,
+  toggleReaderPostCommentLike
+)
+
 router.get(
   '/user/:username',
   requireUser,
