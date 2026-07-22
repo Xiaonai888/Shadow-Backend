21a950e6a4f653dbbaf318bf14b6b7ca8bdb7b82
Xiaonai <xiaonai.ns1@gmail.com>
2026-07-22 20:25:50 +0700

    Update stories.controller.js

--- src/controllers/stories.controller.js
+++ src/controllers/stories.controller.js
@@ -4,7 +4,7 @@ import { blockedWordsWarningPayload, findBlockedWordsInContent } from '../utils/
 const ALLOWED_LANGUAGES = ['Khmer', 'English', 'Chinese', 'Japanese', 'Korean']
 const ALLOWED_UPDATE_DAYS = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
 const ALLOWED_STORY_STATUSES = ['New', 'Ongoing', 'Completed']
-const ALLOWED_STORY_TYPES = ['novel', 'manga']
+const ALLOWED_STORY_TYPES = ['novel', 'manga', 'chat_story']
 const ALLOWED_UNLOCK_METHODS = ['gem', 'voucher', 'story_card', 'free_item']
 const MIN_EPISODE_CHARACTERS = 1500
 const MAX_EPISODE_CHARACTERS = 30000
