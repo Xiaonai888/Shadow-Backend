18ad916fe4cdb333018188cff4c254d6bbc1809a
Xiaonai <xiaonai.ns1@gmail.com>
2026-07-23 14:55:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -63,6 +63,7 @@ import readerPostsRoutes from './src/routes/readerPosts.routes.js'
 import readingProgressRoutes from './src/routes/readingProgress.routes.js'
 import shareProfileRoutes from './src/routes/shareProfile.routes.js'
 import adminChatStoryGalleryRoutes from './src/routes/adminChatStoryGallery.routes.js'
+import adminMediaLibraryRoutes from './src/routes/adminMediaLibrary.routes.js'
 
 dotenv.config()
 
@@ -180,6 +181,7 @@ app.use('/api/admin/notifications', adminNotificationsRoutes)
 app.use('/api/mails', readerActionSpamGuard, readerMailsRoutes)
 app.use('/api/admin/stories', adminStoriesRoutes)
 app.use('/api/admin/chat-story-gallery', adminChatStoryGalleryRoutes)
+app.use('/api/admin/media-library', adminMediaLibraryRoutes)
 app.use('/api/admin/ranking', adminRankingRoutes)
 app.use('/api/advertisements', advertisementsRoutes)
 app.use('/api/admin/block-list', adminBlockListRoutes)
