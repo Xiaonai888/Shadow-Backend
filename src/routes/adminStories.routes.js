4f8835aaa7fe8809bc0881d3b41ef5ae746456d5
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-06 22:06:17 +0700

    Update adminStories.routes.js

--- src/routes/adminStories.routes.js
+++ src/routes/adminStories.routes.js
@@ -8,6 +8,7 @@ import {
   issueStoryWarning,
   updateAuthorAdminStatus,
   updateStoryAdminVisibility,
+  downloadAdminStoryMedia
 } from '../controllers/adminStories.controller.js'
 import { requireAdmin } from '../middleware/auth.middleware.js'
 
@@ -18,6 +19,7 @@ router.get('/update-activity', requireAdmin, getAdminStoryUpdateActivity)
 router.get('/picker', requireAdmin, getAdminStoryPicker)
 router.get('/', requireAdmin, getAdminStories)
 router.get('/:storyId', requireAdmin, getAdminStoryById)
+router.get('/:storyId/media/:mediaType/:mediaIndex/download', requireAdmin, downloadAdminStoryMedia)
 router.patch('/:storyId/visibility', requireAdmin, updateStoryAdminVisibility)
 router.post('/:storyId/warnings', requireAdmin, issueStoryWarning)
 router.patch('/authors/:authorId/status', requireAdmin, updateAuthorAdminStatus)
