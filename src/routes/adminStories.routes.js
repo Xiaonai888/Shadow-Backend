9ba2d6fe15dd21d6c2ec5922250afa02c0b1cfb3
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-20 16:53:39 +0700

    Update adminStories.routes.js

--- src/routes/adminStories.routes.js
+++ src/routes/adminStories.routes.js
@@ -10,18 +10,22 @@ import {
   updateStoryAdminVisibility,
   downloadAdminStoryMedia
 } from '../controllers/adminStories.controller.js'
-import { requireAdmin } from '../middleware/auth.middleware.js'
+import { requireAdminPermission } from '../middleware/adminPermission.middleware.js'
 
 const router = express.Router()
 
-router.get('/overview', requireAdmin, getAdminStoriesOverview)
-router.get('/update-activity', requireAdmin, getAdminStoryUpdateActivity)
-router.get('/picker', requireAdmin, getAdminStoryPicker)
-router.get('/', requireAdmin, getAdminStories)
-router.get('/:storyId', requireAdmin, getAdminStoryById)
-router.get('/:storyId/media/:mediaType/:mediaIndex/download', requireAdmin, downloadAdminStoryMedia)
-router.patch('/:storyId/visibility', requireAdmin, updateStoryAdminVisibility)
-router.post('/:storyId/warnings', requireAdmin, issueStoryWarning)
-router.patch('/authors/:authorId/status', requireAdmin, updateAuthorAdminStatus)
+const viewStories = requireAdminPermission('stories.view')
+const manageStories = requireAdminPermission('stories.manage')
+
+router.get('/overview', viewStories, getAdminStoriesOverview)
+router.get('/update-activity', viewStories, getAdminStoryUpdateActivity)
+router.get('/picker', viewStories, getAdminStoryPicker)
+router.get('/', viewStories, getAdminStories)
+router.get('/:storyId', viewStories, getAdminStoryById)
+router.get('/:storyId/media/:mediaType/:mediaIndex/download', viewStories, downloadAdminStoryMedia)
+
+router.patch('/:storyId/visibility', manageStories, updateStoryAdminVisibility)
+router.post('/:storyId/warnings', manageStories, issueStoryWarning)
+router.patch('/authors/:authorId/status', manageStories, updateAuthorAdminStatus)
 
 export default router
