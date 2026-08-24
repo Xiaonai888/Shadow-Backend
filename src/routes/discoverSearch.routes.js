8f6df7cc41f5bf581177ca486019f5f0fb66d6c8
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-24 17:33:12 +0700

    Update discoverSearch.routes.js

--- src/routes/discoverSearch.routes.js
+++ src/routes/discoverSearch.routes.js
@@ -1,10 +1,14 @@
 import express from 'express'
 import { searchDiscover } from '../controllers/discoverSearch.controller.js'
-import { recordDiscoverSearchClick } from '../controllers/discoverSearchAnalytics.controller.js'
+import {
+  recordDiscoverSearchClick,
+  recordDiscoverSearchEvent,
+} from '../controllers/discoverSearchAnalytics.controller.js'
 
 const router = express.Router()
 
 router.get('/', searchDiscover)
+router.post('/analytics', recordDiscoverSearchEvent)
 router.post('/click', recordDiscoverSearchClick)
 
 export default router
