4ef0ff3b2da487b03e20f3e1789da31ed48c877a
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-24 16:05:46 +0700

    Add route to record discover search clicks

--- src/routes/discoverSearch.routes.js
+++ src/routes/discoverSearch.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { searchDiscover } from '../controllers/discoverSearch.controller.js'
+import { recordDiscoverSearchClick } from '../controllers/discoverSearchAnalytics.controller.js'
 
 const router = express.Router()
 
 router.get('/', searchDiscover)
+router.post('/click', recordDiscoverSearchClick)
 
 export default router
