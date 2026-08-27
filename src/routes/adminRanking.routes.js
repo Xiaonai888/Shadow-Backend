be5c5f240944fd6184648f5a66434c4ff5fd8f3f
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-27 18:22:35 +0700

    Update adminRanking.routes.js

--- src/routes/adminRanking.routes.js
+++ src/routes/adminRanking.routes.js
@@ -3,6 +3,7 @@ import {
   getAdminAuthorRanking,
   getAdminEpisodeRanking,
   getAdminGenreRanking,
+  getAdminIncomeRanking,
   getAdminStoryRanking,
   getHiddenRankingItems,
   updateStoryRankingVisibility,
@@ -15,6 +16,7 @@ router.get('/stories', requireAdmin, getAdminStoryRanking)
 router.get('/genres', requireAdmin, getAdminGenreRanking)
 router.get('/authors', requireAdmin, getAdminAuthorRanking)
 router.get('/episodes', requireAdmin, getAdminEpisodeRanking)
+router.get('/income', requireAdmin, getAdminIncomeRanking)
 router.get('/hidden', requireAdmin, getHiddenRankingItems)
 router.patch('/stories/:storyId/visibility', requireAdmin, updateStoryRankingVisibility)
 
