d84739a81fef227ddd485164abee9f49aa872a8d
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-27 13:16:30 +0700

    Refactor adminEvents.routes to list active events
    Removed admin authentication middleware and associated event routes.

--- src/routes/adminEvents.routes.js
+++ src/routes/adminEvents.routes.js
@@ -1,17 +1,8 @@
 import express from 'express'
-import { requireAdmin } from '../middleware/auth.middleware.js'
-import {
-  createEvent,
-  deleteEvent,
-  listAdminEvents,
-  updateEvent,
-} from '../controllers/events.controller.js'
+import { listActiveEvents } from '../controllers/events.controller.js'
 
 const router = express.Router()
 
-router.get('/', requireAdmin, listAdminEvents)
-router.post('/', requireAdmin, createEvent)
-router.patch('/:eventId', requireAdmin, updateEvent)
-router.delete('/:eventId', requireAdmin, deleteEvent)
+router.get('/', listActiveEvents)
 
 export default router
