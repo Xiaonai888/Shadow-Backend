8982ff0e1780256bc89a41febdf689a801072e49
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-28 22:58:18 +0700

    Update visitorAnalytics.routes.js

--- src/routes/visitorAnalytics.routes.js
+++ src/routes/visitorAnalytics.routes.js
@@ -1,8 +1,10 @@
 import express from 'express'
 import { trackAnonymousVisitor } from '../controllers/visitorAnalytics.controller.js'
+import { trackStorySectionRankEvent } from '../controllers/storySectionRank.controller.js'
 
 const router = express.Router()
 
 router.post('/track', trackAnonymousVisitor)
+router.post('/story-section-rank', trackStorySectionRankEvent)
 
 export default router
