25c2b7d55cf41aece27eb7ed0dfff058c1b8e3f4
Xiaonai <xiaonai.ns1@gmail.com>
2026-08-29 21:09:43 +0700

    Update storyTranslation.routes.js

--- src/routes/storyTranslation.routes.js
+++ src/routes/storyTranslation.routes.js
@@ -3,6 +3,12 @@ import { translateStory } from '../controllers/storyTranslation.controller.js'
 
 const router = express.Router()
 
-router.post('/', translateStory)
+router.post('/', (req, res) => {
+  if (process.env.STORY_TRANSLATION_ENABLED !== 'true') {
+    return res.status(503).json({ ok: false, code: 'STORY_TRANSLATION_DISABLED' })
+  }
+
+  return translateStory(req, res)
+})
 
 export default router
