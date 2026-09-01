94fab988c18f7b25f3a3c865cb7eb137424f6592
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-01 09:01:48 +0700

    Update adminIncome.routes.js

--- src/routes/adminIncome.routes.js
+++ src/routes/adminIncome.routes.js
@@ -3,6 +3,7 @@ import { requireAdmin } from '../middleware/auth.middleware.js'
 import { getAdminDiamondGifts } from '../controllers/adminDiamondGifts.controller.js'
 import { getAdminAuthorPageIncome } from '../controllers/adminAuthorPageIncome.controller.js'
 import { getAdminShadowMallIncome } from '../controllers/adminShadowMallIncome.controller.js'
+import { streamAdminIncomeEvents } from '../services/adminIncomeEvents.service.js'
 import {
   generateAdminAuthorPayouts,
   getAdminAuthorPayouts,
@@ -13,6 +14,7 @@ import {
 
 const router = express.Router()
 
+router.get('/events', requireAdmin, streamAdminIncomeEvents)
 router.get('/summary', requireAdmin, getAdminIncomeSummary)
 router.get('/episode-sales', requireAdmin, getAdminEpisodeSales)
 router.get('/diamond-gifts', requireAdmin, getAdminDiamondGifts)
