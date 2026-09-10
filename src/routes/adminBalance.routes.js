fd6d990efa8142f49b1c2408841ae2a03aa7cb88
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-10 22:25:53 +0700

    Update adminBalance.routes.js

--- src/routes/adminBalance.routes.js
+++ src/routes/adminBalance.routes.js
@@ -1,7 +1,10 @@
 import express from 'express'
 import { requireAdmin } from '../middleware/auth.middleware.js'
 import { createSpamGuard } from '../middleware/spamGuard.middleware.js'
-import { getAdminBalanceWallets } from '../controllers/adminBalance.controller.js'
+import {
+  getAdminBalanceDiamondHistory,
+  getAdminBalanceWallets,
+} from '../controllers/adminBalance.controller.js'
 
 const router = express.Router()
 
@@ -16,6 +19,13 @@ router.get(
   requireAdmin,
   adminBalanceReadGuard,
   getAdminBalanceWallets
+)
+
+router.get(
+  '/:userId/diamond-history',
+  requireAdmin,
+  adminBalanceReadGuard,
+  getAdminBalanceDiamondHistory
 )
 
 export default router
