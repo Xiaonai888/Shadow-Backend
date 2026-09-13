4933bfbaab10e7bffc26550b59f0954e9a29a68b
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-13 13:02:41 +0700

    Update publicStories.routes.js

--- src/routes/publicStories.routes.js
+++ src/routes/publicStories.routes.js
@@ -12,12 +12,27 @@ import {
 } from '../controllers/publicStories.controller.js'
 import { getPublicWeeklyUpdates } from '../controllers/weeklyUpdates.controller.js'
 import { getPublicStoryUpdates } from '../controllers/storyUpdates.controller.js'
+import { createSpamGuard } from '../middleware/spamGuard.middleware.js'
+import { createRateLimit } from '../middleware/rateLimit.middleware.js'
 import {
   cachePublicStoriesResponse,
   invalidatePublicStoriesCache,
 } from '../services/publicStoriesResponseCache.service.js'
 
 const router = express.Router()
+
+const publicStoriesListCacheHitLimit = createRateLimit({
+  key: 'public-stories-list-cache-hit',
+  windowMs: 60 * 1000,
+  max: 1500,
+  message: 'Too many requests. Please wait before trying again.',
+})
+
+const publicStoriesListReadSpamGuard = createSpamGuard({
+  scope: 'reader_read',
+  threshold: 300,
+  windowSeconds: 60,
+})
 
 function invalidatePublicStoriesAfterCountedView(req, res, next) {
   const originalJson = res.json.bind(res)
@@ -38,7 +53,13 @@ function invalidatePublicStoriesAfterCountedView(req, res, next) {
   next()
 }
 
-router.get('/stories', cachePublicStoriesResponse, getPublicStories)
+router.get(
+  '/stories',
+  publicStoriesListCacheHitLimit,
+  cachePublicStoriesResponse,
+  publicStoriesListReadSpamGuard,
+  getPublicStories
+)
 router.get('/weekly-updates', getPublicWeeklyUpdates)
 router.get('/story-updates', getPublicStoryUpdates)
 router.get('/shadow-exclusive/stories', getPublicShadowExclusiveStories)
