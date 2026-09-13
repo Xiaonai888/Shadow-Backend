63748840fba3af80ef85df9a58c1838b4c6d4383
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-13 11:53:21 +0700

    Update adminExclusive.routes.js

--- src/routes/adminExclusive.routes.js
+++ src/routes/adminExclusive.routes.js
@@ -8,17 +8,56 @@ import {
   updateShadowExclusiveSections,
 } from '../controllers/adminExclusive.controller.js'
 import { requireAdminPermission } from '../middleware/adminPermission.middleware.js'
+import { invalidatePublicStoriesCache } from '../services/publicStoriesResponseCache.service.js'
 
 const router = express.Router()
 
 const viewExclusive = requireAdminPermission('shadow_exclusive.view')
 const manageExclusive = requireAdminPermission('shadow_exclusive.manage')
 
+function invalidatePublicStoriesAfterMutation(req, res, next) {
+  res.once('finish', () => {
+    if (
+      res.statusCode >= 200 &&
+      res.statusCode < 300
+    ) {
+      invalidatePublicStoriesCache()
+    }
+  })
+
+  next()
+}
+
 router.get('/stories', viewExclusive, listAdminExclusiveStories)
-router.patch('/stories/:storyId/request', manageExclusive, requestShadowExclusive)
-router.patch('/stories/:storyId/approve', manageExclusive, approveShadowExclusive)
-router.patch('/stories/:storyId/reject', manageExclusive, rejectShadowExclusive)
-router.patch('/stories/:storyId/remove', manageExclusive, removeShadowExclusive)
-router.patch('/stories/:storyId/sections', manageExclusive, updateShadowExclusiveSections)
+router.patch(
+  '/stories/:storyId/request',
+  manageExclusive,
+  invalidatePublicStoriesAfterMutation,
+  requestShadowExclusive
+)
+router.patch(
+  '/stories/:storyId/approve',
+  manageExclusive,
+  invalidatePublicStoriesAfterMutation,
+  approveShadowExclusive
+)
+router.patch(
+  '/stories/:storyId/reject',
+  manageExclusive,
+  invalidatePublicStoriesAfterMutation,
+  rejectShadowExclusive
+)
+router.patch(
+  '/stories/:storyId/remove',
+  manageExclusive,
+  invalidatePublicStoriesAfterMutation,
+  removeShadowExclusive
+)
+router.patch(
+  '/stories/:storyId/sections',
+  manageExclusive,
+  invalidatePublicStoriesAfterMutation,
+  updateShadowExclusiveSections
+)
 
 export default router
