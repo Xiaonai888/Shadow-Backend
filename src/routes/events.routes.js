0296ab8595528c952a9c9ad2d4565a43fecf6811
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-14 21:47:37 +0700

    Update events.routes.js

--- src/routes/events.routes.js
+++ src/routes/events.routes.js
@@ -1,7 +1,15 @@
 import express from 'express'
 import { listActiveEvents } from '../controllers/events.controller.js'
+import { createRateLimit } from '../middleware/rateLimit.middleware.js'
+
+const publicEventsRateLimit = createRateLimit({
+  key: 'public_events',
+  windowMs: 60000,
+  max: 120,
+})
 
 const router = express.Router()
-router.get('/', listActiveEvents)
+router.get('/', publicEventsRateLimit, listActiveEvents)
 
 export default router
+
