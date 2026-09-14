2b641065cf10afa8683e629c9b52cab0a8724034
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-14 22:39:22 +0700

    Update adminWork.routes.js

--- src/routes/adminWork.routes.js
+++ src/routes/adminWork.routes.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import { requireAdmin } from '../middleware/auth.middleware.js'
-import { getWorkIncidents } from '../controllers/adminWork.controller.js'
+import {
+  getWorkIncidents,
+  streamWorkIncidents,
+} from '../controllers/adminWork.controller.js'
 
 const router = express.Router()
 
 router.use(requireAdmin)
 
+router.get('/events', streamWorkIncidents)
 router.get('/incidents', getWorkIncidents)
 
 export default router
