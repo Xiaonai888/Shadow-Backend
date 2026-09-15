f069e0d14f8307325f6155d4eb9c01ce1b1e4ad2
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-15 07:23:28 +0700

    Update adminWork.routes.js

--- src/routes/adminWork.routes.js
+++ src/routes/adminWork.routes.js
@@ -5,14 +5,19 @@ import {
   streamWorkIncidents,
 } from '../controllers/adminWork.controller.js'
 import { getAdminWorkKillSwitches, setAdminWorkKillSwitch } from '../controllers/adminWorkKillSwitch.controller.js'
+import { createSecurityGate } from '../middleware/securityGate.middleware.js'
 
 const router = express.Router()
+const killSwitchEnableGate = createSecurityGate({ gateId: 'kill_switch_enable', roles: ['owner'], allowOwner: true, allowInSafeMode: true })
+const killSwitchDisableGate = createSecurityGate({ gateId: 'kill_switch_disable', roles: ['owner'], allowOwner: true, allowInSafeMode: false })
+const protectKillSwitchMutation = (req, res, next) =>
+  (req.body?.enabled === false ? killSwitchDisableGate : killSwitchEnableGate)(req, res, next)
 
 router.use(requireAdmin)
 
 router.get('/events', streamWorkIncidents)
 router.get('/incidents', getWorkIncidents)
 router.get('/kill-switches', getAdminWorkKillSwitches)
-router.put('/kill-switches', setAdminWorkKillSwitch)
+router.put('/kill-switches', protectKillSwitchMutation, setAdminWorkKillSwitch)
 
 export default router
