d62a11846f2d6b45080e10e73c1d8fc7a518d001
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-17 20:26:35 +0700

    Update discoverStories.routes.js

--- src/routes/discoverStories.routes.js
+++ src/routes/discoverStories.routes.js
@@ -3,6 +3,10 @@ import {
   getDiscoverStoriesFeed,
 } from '../controllers/discoverStories.controller.js'
 import {
+  getDiscoverStoryReactionStatus,
+  toggleDiscoverStoryReaction,
+} from '../controllers/discoverStoryReactions.controller.js'
+import {
   requireUser,
 } from '../middleware/user.middleware.js'
 
@@ -12,6 +16,18 @@ router.get(
   '/feed',
   requireUser,
   getDiscoverStoriesFeed
+)
+
+router.get(
+  '/:sourceType/:storyId/reaction',
+  requireUser,
+  getDiscoverStoryReactionStatus
+)
+
+router.post(
+  '/:sourceType/:storyId/reaction',
+  requireUser,
+  toggleDiscoverStoryReaction
 )
 
 export default router
