56d64237da72bbca2f0f0bd1fcf4987f9e66d689
Xiaonai <xiaonai.ns1@gmail.com>
2026-09-22 13:22:52 +0700

    Update adminEvents.routes.js

--- src/routes/adminEvents.routes.js
+++ src/routes/adminEvents.routes.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import author100PercentRoutes from './adminAuthor100PercentEvent.routes.js'
 import { requireAdminPermission } from '../middleware/adminPermission.middleware.js'
 import {
   createEvent,
@@ -12,6 +13,7 @@ const router = express.Router()
 const eventPermission = requireAdminPermission('monthly_vote.view')
 
 router.get('/', eventPermission, listAdminEvents)
+router.use('/100-percent', eventPermission, author100PercentRoutes)
 router.post('/', eventPermission, createEvent)
 router.patch('/:eventId', eventPermission, updateEvent)
 router.delete('/:eventId', eventPermission, deleteEvent)
